f8e47b22940ca965ecfff29c4904c8e262b1a7f4
mgamerz <mgamerz2@gmail.com>
2014-09-09 21:24:32 -0600

    Added fetch_mail code.

--- GuerrillaTester.py
+++ GuerrillaTester.py
@@ -1,23 +1,33 @@
 __author__ = 'Mgamerz'
 
 import unittest
-import guerrilla
+from guerrilla import GuerrillaAPI
 import re
 
 
 class GuerrillaTests(unittest.TestCase):
     def test_get_email(self):
-        apisession = guerrilla.GuerrillaAPI()
+        apisession = GuerrillaAPI()
         apisession.get_email_address()
         self.assertTrue(re.match(r"[^@]+@[^@]+\.[^@]+", apisession.EMAIL))
         #test
 
     def test_original_inbox(self):
-        apisession = guerrilla.GuerrillaAPI()
+        apisession = GuerrillaAPI()
         self.assertEquals(None, apisession.get_email_list())
         apisession.get_email_address()
         emails = apisession.get_email_list()
         self.assertNotEquals(None, apisession.get_email_list())
+
+    def test_fetch(self):
+        apisession = GuerrillaAPI()
+        self.assertEquals(None, apisession.get_email_list())
+        apisession.get_email_address()
+        emails = apisession.get_email_list()
+        self.assertNotEquals(None, apisession.get_email_list())
+        for mail in emails:
+            message = apisession.fetch_mail(mail['mail_id'])
+            GuerrillaAPI.print_email(message)
 
 if __name__ == '__main__':
     unittest.main()
--- guerrilla.py
+++ guerrilla.py
@@ -8,7 +8,7 @@ class GuerrillaAPI:
 
     def __init__(self):
         self.GUERRILLA_URL = 'https://api.guerrillamail.com/ajax.php'
-        self.PYTHON_AGENT = 'Mgamerz_iOS_Feasibility_Test_via_Python'
+        self.PYTHON_AGENT = 'PythonWrapperGM'
         self.IP_ADDRESS = '127.0.0.1'
         self.LANG = 'en'
         self.SESSION_COOKIE = None
@@ -20,12 +20,13 @@ class GuerrillaAPI:
 
     def get_email_address(self):
         self.URL_PARAMS['f'] = 'get_email_address'
+        #self.SESSION_COOKIE = dict(PHPSESSID='8oiuab186urudli5gh32pkb8t7')
+        #email = requests.get(self.GUERRILLA_URL, params=self.URL_PARAMS, cookies=self.SESSION_COOKIE)
         email = requests.get(self.GUERRILLA_URL, params=self.URL_PARAMS)
         self.SESSION_COOKIE = dict(PHPSESSID=email.cookies['PHPSESSID'])
         email_json = email.json()
         self.EMAIL = email_json['email_addr']
         self.EMAIL_ALIAS = email_json['alias']
-        print('Got address:', self.EMAIL)
         return
 
     def get_email_list(self):
@@ -36,39 +37,38 @@ class GuerrillaAPI:
         url_params['f'] = 'get_email_list'
         url_params['offset'] = '0'
         email_list = requests.get(self.GUERRILLA_URL, params=url_params, cookies=self.SESSION_COOKIE)
-        print(email_list.json())
         return email_list.json()['list']
 
-    def print_email(self, mail_item):
+    @staticmethod
+    def print_email(mail_item):
         mail_from = mail_item['mail_from']
         mail_subject = mail_item['mail_subject']
         mail_text = mail_item['mail_excerpt']
+        if 'mail_body' in mail_item:
+            mail_text = mail_item['mail_body']
         print('From:', mail_from)
         print('Subject:', mail_subject)
         print(mail_text)
         print('\n----------------------------------------')
         return
 
-
-    def fetch_mail(self, id):
+    def fetch_mail(self, mail_id):
         """
         Fetches the full information about an email.
         :param id: The ID of the email.
         :return: A json array containing the email, or None if it was not found.
         """
-
         if not self.SESSION_COOKIE:
             print("ERROR: You need to get the email address first to get a session token.")
             return
         url_params = copy.deepcopy(self.URL_PARAMS)
         url_params['f'] = 'fetch_email'
-        url_params['id'] = id
+        url_params['email_id'] = mail_id
         email = requests.get(self.GUERRILLA_URL, params=url_params, cookies=self.SESSION_COOKIE)
         if not email:
             print('RETURNED FALSE!')
             return
-
-
+        return email.json()
         #print(email.text)
         #return email_list.json()['list']
 
